c56850c8e4dae623e99ebc381d016bc61b03c22b
chlwhdgh <163220452+chlwhdgh@users.noreply.github.com>
2024-03-27 02:54:46 +0000

    끝!

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// 필요 모듈 임포트
+const port = 3000,
+    http = require('http'),
+    httpStatus = require('http-status-codes'),
+    router = require('./router'),
+    contentTypes = require('./content-types'),
+    utils = require('./utils');
+
+// Listen 7.8 (p. 127-128)
+// GET 라우트
+router.get("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+});
+router.get("/courses", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/courses.html", res);
+});
+router.get("/contact", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.contact", res);
+});
+
+// POST 라우트
+router.post("/contact", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/thanks.html", res);
+});
+
+// 에셋 라우트
+// CSS 라우트
+router.get("/bootstrap.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/bootstrap.css", res);
+});
+router.get("/style.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("public/css/style.css", res);
+});
+
+// JS 라우트
+// IMG 라우트
+router.get("/people.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/people.jpg", res);
+});
+router.get("/product.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/product.jpg", res);
+});
+router.get("/graph.png", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/graph.png", res);
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`Server at: http://localhost:${port}`);
